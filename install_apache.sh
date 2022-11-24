83533fbfd22a042b6c7915ee9418b6a664056027
PaulMarvin-Cloud <oforiyaw.marvin@gmail.com>
2022-11-24 22:54:26 +0000

    First main commit

---
+++ install_apache.sh
@@ -0,0 +1,8 @@
+# Apache Script
+#!/bin/bash
+
+yum update -y
+yum install -y httpd.x86_64
+systemctl start httpd.service
+systemctl enable httpd.service
+echo "Hello World from $(hostname -f)" > /var/www/html/index.html
